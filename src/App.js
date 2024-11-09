18bd2fb96908aa123a5c6895025c294915eaf6a6
Harsh Singh <alexlin2922@gmail.com>
2024-11-09 14:09:43 +0530

    please work

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline ">
-      Hello world! 245343
+    hopefully working
     </h1>
     </div>
   );
